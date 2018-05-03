fc4b2eba709935f23f08d9d9220012ed9ff0028b
martinezk <misskristinamartinez@gmail.com>
2018-05-03 10:26:33 -0400

    Add association between link and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430154059) do
+ActiveRecord::Schema.define(version: 20180503142253) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
